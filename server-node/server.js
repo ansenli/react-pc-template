b6b9d0fc715586594e00b5fe5a0ad26d2ec37c07
01728662 <ansenli@163.com>
2019-06-20 10:26:59 +0800

    配置了静态服务器

--- server-node/server.js
+++ server-node/server.js
@@ -1,5 +1,7 @@
 
 const Router = require('koa-router')();
+const path = require('path');
+const static = require('koa-static');
 const Koa = require('koa');
 const App = new Koa();
 
@@ -9,16 +11,19 @@ let home = require('./routes/home');
 let login = require('./routes/login');
 let register = require('./routes/register');
 let user = require('./routes/user');
-
-
+//设置静态资源的路径 
+const staticPath = '../build';
 /*装载所有子路由*/
 Router.use('/home',home.routes());
-Router.use('/login',login.routes());
+Router.use('/login',login.routes()); // 打包后对应的路径要修改成 /api/login
 Router.use('/register',register.routes());
 Router.use('/user',user.routes());
 /* 加载路由中间件 */
 App.use(Router.routes());
 
+/* 配置静态服务器 */
+
+App.use(static(path.join(__dirname,staticPath)));
 /* 启动服务绑定端口 */
 let server = App.listen(3080,()=>{
   let host = server.address().address;
